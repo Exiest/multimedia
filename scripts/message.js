ff999071720514c4e5beacf70a9c9dc98f9400da
Dmytro Lyn <dlin3738@gmail.com>
2021-11-22 15:52:25 +0200

    pr

---
+++ scripts/message.js
@@ -0,0 +1,19 @@
+var Ucren = require( "lib/ucren" );
+
+exports.postMessage = function( message, to ){
+	var messages = [].slice.call( arguments, 0 ),
+		splitIndex = messages.length - 1;
+
+	to = messages[ splitIndex ];
+	messages.slice( 0, splitIndex );
+
+	Ucren.dispatch( to, messages );
+};
+
+exports.addEventListener = function( from, fn ){
+	Ucren.dispatch( from, fn );
+};
+
+exports.removeEventListener = function( from, fn ){
+	Ucren.dispatch.remove( from, fn );
+};
